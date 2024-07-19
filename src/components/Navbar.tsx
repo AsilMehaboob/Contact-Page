706eb657bceba098cc57a57aca301c92c4a19aef
AsilMehaboob <138790394+AsilMehaboob@users.noreply.github.com>
2024-07-19 22:40:15 +0530

    third  commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const NavBar: React.FC = () => {
   return (
     <Box as="header" py={4} px={6} borderBottomWidth={1}>
       <Flex justifyContent="space-between" alignItems="center">
-        <Heading as="h1" size="sm">CommBase</Heading>
+        <Heading as="h1" size="sm">Contact Us</Heading>
         <Flex flex="1" justifyContent="center">
           <HStack spacing={12}>
             <Link href="#"><Text fontSize="sm">Home</Text></Link>
